9eddbd6ddbba12905de3a4527d47d020cb4da83f
mahmouddev <m.abdelmoniem@kortoba.com>
2024-12-22 05:29:46 +0200

    FIX : fix logo url

--- templates/element/header.php
+++ templates/element/header.php
@@ -82,7 +82,7 @@
           <div class="col-sm-4 col-lg-3 text-center text-sm-start">
             <div class="main-logo">
               <a href="/">
-                <img src="images/logo.png" alt="logo" class="img-fluid">
+                <img src="/images/logo.png" alt="logo" class="img-fluid">
               </a>
             </div>
           </div>
