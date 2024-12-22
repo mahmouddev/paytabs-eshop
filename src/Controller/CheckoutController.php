24731105ec143135cfc05da6c7ef0b9201a45fd7
mahmouddev <m.abdelmoniem@kortoba.com>
2024-12-22 03:04:55 +0200

    UPDATE : add basic checkout controller
    - to handle any checkout related requests , i added this controller for logged in users action , also to apply SRP and separation of concerns

---
+++ src/Controller/CheckoutController.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Checkout Controller
+ *
+ */
+class CheckoutController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $this->set([]);
+    }
+}
