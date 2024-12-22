e1f907e7241439f08e94505263938a50bd7a3f62
mahmouddev <m.abdelmoniem@kortoba.com>
2024-12-22 02:36:41 +0200

    UPDATE : install Store Template
    This is free templat for simple ecomerce app , you can find all it's resources on
    ```
    https://templatesjungle.gumroad.com/l/foodmart-bootstrap-html-template
    ```

---
+++ webroot/js/script.js
@@ -0,0 +1,81 @@
+(function($) {
+
+  "use strict";
+
+  var initPreloader = function() {
+    $(document).ready(function($) {
+    var Body = $('body');
+        Body.addClass('preloader-site');
+    });
+    $(window).load(function() {
+        $('.preloader-wrapper').fadeOut();
+        $('body').removeClass('preloader-site');
+    });
+  }
+
+
+  var initSwiper = function() {
+
+
+
+    var products_swiper = new Swiper(".products-carousel", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".products-carousel-next",
+        prevEl: ".products-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1500: {
+          slidesPerView: 6,
+        },
+      }
+    });
+  }
+
+  var initProductQty = function(){
+
+    $('.product-qty').each(function(){
+
+      var $el_product = $(this);
+      var quantity = 0;
+
+      $el_product.find('.quantity-right-plus').click(function(e){
+          e.preventDefault();
+          var quantity = parseInt($el_product.find('#quantity').val());
+          $el_product.find('#quantity').val(quantity + 1);
+      });
+
+      $el_product.find('.quantity-left-minus').click(function(e){
+          e.preventDefault();
+          var quantity = parseInt($el_product.find('#quantity').val());
+          if(quantity>0){
+            $el_product.find('#quantity').val(quantity - 1);
+          }
+      });
+
+    });
+
+  }
+
+
+  // document ready
+  $(document).ready(function() {
+
+    initPreloader();
+    initSwiper();
+    initProductQty();
+
+  }); // End of a document
+
+})(jQuery);
